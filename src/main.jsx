4a7dc13af232ca4a51582783309cc000c02e9425
Niceas-Martins <niceasmartins@outlook.com>
2025-04-23 16:18:43 -0300

    Corrigindo rotas para GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import "./Style/global.css";
 import Home from "./pages/Home";
@@ -11,20 +11,20 @@ import Exame_Do_Paciente from './pages/Home/Exame_Do_Paciente';
 import Exame from './pages/Home/Exame';
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <Router>
     <Routes>
-      <Route path="/Uni_Rastrei" element={<Home />} />
+      <Route path="/" element={<Home />} />
       <Route path="/Dashboard" element={<Dashboard />} />
       <Route path="/Novo_Paciente" element={<Novo_Paciente />} />
       <Route path="/Calendario" element={<Calendario />} />
-      <Route path="Paciente" element={<Paciente />} />
-      <Route path="/Historico_Do_Paciente" element={<Historico_Do_Paciente/>} />
-      <Route path="/Exame_Do_Paciente" element={<Exame_Do_Paciente/>}/>
-      <Route path="/Exame/0" element={<Exame/>} />
-      <Route path="/Exame/1" element={<Exame/>} />
-      <Route path="/Exame/2" element={<Exame/>} />
-      <Route path="/Exame/3" element={<Exame/>} />
-      <Route path="/Exame/4" element={<Exame/>} />
-     </Routes>
-  </BrowserRouter>
+      <Route path="/Paciente" element={<Paciente />} />
+      <Route path="/Historico_Do_Paciente" element={<Historico_Do_Paciente />} />
+      <Route path="/Exame_Do_Paciente" element={<Exame_Do_Paciente />} />
+      <Route path="/Exame/0" element={<Exame />} />
+      <Route path="/Exame/1" element={<Exame />} />
+      <Route path="/Exame/2" element={<Exame />} />
+      <Route path="/Exame/3" element={<Exame />} />
+      <Route path="/Exame/4" element={<Exame />} />
+    </Routes>
+  </Router>
 );
